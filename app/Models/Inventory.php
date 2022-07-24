bbaa0359a77f1d36f64e820a9a9444dc9847f78e
Saker-Hossain <sakerhossain2017@gmail.com>
2022-07-24 11:47:17 +0600

    Customer, Product, Inventory and InventoryProduct created and eeditted

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Inventory extends Model
 {
     use HasFactory;
+    protected $table = 'inventories';
+    protected $fillable = [
+        'date',
+        'bill_no',
+        'customer_id',
+        'total_discount',
+        'total_bill_amount',
+        'due_amount',
+        'paid_amount',
+    ];
+
+    public function customers()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id', 'id');
+    }
 }
--- app/Models/InventoryProduct.php
+++ app/Models/InventoryProduct.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class InventoryProduct extends Model
 {
     use HasFactory;
+    protected $table = 'inventory_products';
+    protected $fillable = [
+        'inventory_id',
+        'product_id',
+        'rate',
+        'qty',
+        'discount',
+    ];
+
+    public function inventory()
+    {
+        return $this->belongsTo(Inventory::class, 'inventory_id', 'id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
 }
